64b47b1ee7d2177d1f9f7e9decf6e820330c37fb
Belcebu <68253856+Belcebu98@users.noreply.github.com>
2022-01-30 13:34:38 -0300

    【﻿Ｉｎｓｔａｌａｃｉｏｎ　ｄｅｌ　Ｂｏｔ】

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+#!/usr/bin/bash
 echo -e '\e[1;32m
 ▀█▀ █▀▀▄ █▀▀ ▀▀█▀▀ █▀▀█ █── █▀▀█ █▀▀▄ █▀▀▄ █▀▀█ 　 ░█▀▄▀█ █▀▀█ █▀▀▄ █──█ █── █▀▀█ █▀▀ 
 ░█─ █──█ ▀▀█ ──█── █▄▄█ █── █▄▄█ █──█ █──█ █──█ 　 ░█░█░█ █──█ █──█ █──█ █── █──█ ▀▀█ 
@@ -7,41 +8,134 @@ echo -e '\e[1;32m
 ░█░█░█ █▀▀ █── █▀▀ ▀▀█ █▄▄█ █▄▄▀ ▀█▀ █──█ ▀▀█ 
 ░█──▀█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀──▀ ▀─▀▀ ▀▀▀ ▀▀▀▀ ▀▀▀'
 
-echo  Se esta ejecutando el proceso de instalacion
-echo  Por favor se paciente
+echoSe esta ejecutando el proceso de instalacion
+echoPor favor se paciente
 
-apt-get update  > /dev/null 2>&1
-apt-get upgrade > /dev/null 2>&1
-apt-get install nodejs > /dev/null 2>&1
-apt-get install libwebp > /dev/null 2>&1
-apt-get install mc > /dev/null 2>&1
-apt-get install ffmpeg > /dev/null 2>&1
-apt-get install wget > /dev/null 2>&1
-apt-get install tesseract > /dev/null 2>&1
-apt-get install nmap > /dev/null 2>&1
-wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true" > /dev/null 2>&1
-npm install -g npm@6 -y  > /dev/null 2>&1
-npm install toml -y > /dev/null 2>&1
-npm i crypto-js -y > /dev/null 2>&1
-npm i moment-timezone -y > /dev/null 2>&1
-npm i axios -y > /dev/null 2>&1
-npm i node-fetch@2.6.1 - npm -y > /dev/null 2>&1
-npm i requests -y > /dev/null 2>&1
-npm install request --save -y > /dev/null 2>&1
-npm i cfonts -y > /dev/null 2>&1
-npm i spinnies > /dev/null 2>&1
-npm i image-to-base64 -y > /dev/null 2>&1
-npm i remove.bg -y > /dev/null 2>&1
-npm i fluent-ffmpeg -y > /dev/null 2>&1
-npm i @adiwajshing/baileys -y > /dev/null 2>&1
-npm i performance-now -y > /dev/null 2>&1
-npm i form-data -y > /dev/null 2>&1
-npm i yt-search -y > /dev/null 2>&1
-npm i google-it@1.6.1 -y > /dev/null 2>&1
-npm i g-i-s -y > /dev/null 2>&1
-npm i hxz-api -y > /dev/null 2>&1
-npm i cheerio -y > /dev/null 2>&1
-npm i qrcode -y > /dev/null 2>&1
+
+apt-get update
+clear
+echo
+echo    Proceso de instalacion en marcha
+apt-get upgrade
+clear
+echo
+echo    Proceso de instalacion en marcha.
+apt-get install nodejs > dev/null 2>&1
+clear
+echo
+echo    Proceso de instalacion en marcha..
+apt-get install libwebp    > /dev/null 2>&1
+clear
+echo
+echo    Proceso de instalacion en marcha...
+apt-get install mc    > /dev/null 2>&1
+clear
+echo
+echo    Proceso de instalacion en marcha
+apt-get install ffmpeg    > /dev/null 2>&1
+clear
+echo
+echo    Proceso de instalacion en marcha.
+apt-get install wget    > /dev/null 2>&1
+clear
+echo
+echo    Proceso de instalacion en marcha..
+apt-get install tesseract    > /dev/null 2>&1
+clear
+echo
+echo    Proceso de instalacion en marcha...
+apt-get install nmap    > /dev/null 2>&1
+clear
+wget -O ~/clearclear/usr/share/tessdata/indcleartraineddata "https://githubclearcom/tesseract-ocr/tessdata/blob/master/indcleartraineddata?raw=true"
+clear
+echo
+echo    Acabando proceso de instalacion
+npm install -g npm@6 -y    > /dev/null 2>&1
+clear
+echo
+echo    Acabando proceso de instalacion.
+npm install toml -y    > /dev/null 2>&1
+clear
+echo
+echo    Acabando proceso de instalacion..
+npm i crypto-js -y    > /dev/null 2>&1
+clear
+echo
+echo    Acabando proceso de instalacion...
+npm i moment-timezone -y    > /dev/null 2>&1
+clear
+echo
+echo    Acabando proceso de instalacion.
+npm i axios -y    > /dev/null 2>&1
+clear
+echo
+echo    Acabando proceso de instalacion..
+npm i node-fetch@2clear6clear1 - npm -y    > /dev/null 2>&1
+clear
+echo
+echo    Acabando proceso de instalacion...
+npm i requests -y    > /dev/null 2>&1
+clear
+echo
+echo    Acabando proceso de instalacion
+npm install request --save -y    > /dev/null 2>&1
+clear
+echo
+echo    Acabando proceso de instalacion.
+npm i cfonts -y    > /dev/null 2>&1
+clear
+echo
+echo    Acabando proceso de instalacion..
+npm i spinnies    > /dev/null 2>&1
+clear
+echo
+echo    Acabando proceso de instalacion...
+npm i image-to-base64 -y    > /dev/null 2>&1
+clear
+echo
+echo    FINALIZANDO
+npm i removeclearbg -y    > /dev/null 2>&1
+clear
+echo
+echo    FINALIZANDO.
+npm i fluent-ffmpeg -y    > /dev/null 2>&1
+clear
+echo
+echo    FINALIZANDO..
+npm i @adiwajshing/baileys -y    > /dev/null 2>&1
+clear
+echo
+echo    FINALIZANDO...
+npm i performance-now -y    > /dev/null 2>&1
+clear
+echo
+echo    FINALIZANDO
+npm i form-data -y    > /dev/null 2>&1
+clear
+echo
+echo    FINALIZANDO.
+npm i yt-search -y    > /dev/null 2>&1
+clear
+echo
+echo    FINALIZANDO..
+npm i google-it@1clear6clear1 -y    > /dev/null 2>&1
+clear
+echo
+echo    FINALIZANDO...
+npm i g-i-s -y    > /dev/null 2>&1
+clear
+echo
+echo    FINALIZANDO
+npm i hxz-api -y    > /dev/null 2>&1
+clear
+echo
+echo    FINALIZANDO.
+npm i cheerio -y    > /dev/null 2>&1
+clear
+echo
+echo    FINALIZANDO..
+npm i qrcode -y    > /dev/null 2>&1
+clear
 
 echo -e '\e[1;32m
 Descarga Completada al 100%
