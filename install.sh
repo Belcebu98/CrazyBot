1717c79234a6e49bfc076730eb0ceda53c4e7759
Belcebu <68253856+Belcebu98@users.noreply.github.com>
2022-01-30 14:12:58 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-#!/usr/bin/bash
 echo -e '\e[1;32m
 ▀█▀ █▀▀▄ █▀▀ ▀▀█▀▀ █▀▀█ █── █▀▀█ █▀▀▄ █▀▀▄ █▀▀█ 　 ░█▀▄▀█ █▀▀█ █▀▀▄ █──█ █── █▀▀█ █▀▀ 
 ░█─ █──█ ▀▀█ ──█── █▄▄█ █── █▄▄█ █──█ █──█ █──█ 　 ░█░█░█ █──█ █──█ █──█ █── █──█ ▀▀█ 
@@ -8,134 +7,38 @@ echo -e '\e[1;32m
 ░█░█░█ █▀▀ █── █▀▀ ▀▀█ █▄▄█ █▄▄▀ ▀█▀ █──█ ▀▀█ 
 ░█──▀█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀──▀ ▀─▀▀ ▀▀▀ ▀▀▀▀ ▀▀▀'
 
-echoSe esta ejecutando el proceso de instalacion
-echoPor favor se paciente
-
-
 apt-get update
-clear
-echo
-echo    Proceso de instalacion en marcha
 apt-get upgrade
-clear
-echo
-echo    Proceso de instalacion en marcha.
-apt-get install nodejs > dev/null 2>&1
-clear
-echo
-echo    Proceso de instalacion en marcha..
-apt-get install libwebp    > /dev/null 2>&1
-clear
-echo
-echo    Proceso de instalacion en marcha...
-apt-get install mc    > /dev/null 2>&1
-clear
-echo
-echo    Proceso de instalacion en marcha
-apt-get install ffmpeg    > /dev/null 2>&1
-clear
-echo
-echo    Proceso de instalacion en marcha.
-apt-get install wget    > /dev/null 2>&1
-clear
-echo
-echo    Proceso de instalacion en marcha..
-apt-get install tesseract    > /dev/null 2>&1
-clear
-echo
-echo    Proceso de instalacion en marcha...
-apt-get install nmap    > /dev/null 2>&1
-clear
-wget -O ~/clearclear/usr/share/tessdata/indcleartraineddata "https://githubclearcom/tesseract-ocr/tessdata/blob/master/indcleartraineddata?raw=true"
-clear
-echo
-echo    Instalando npm
-npm install -g npm@6 -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando toml
-npm install toml -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando crypto-js
-npm i crypto-js -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando moment-timezone
-npm i moment-timezone -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando axios
-npm i axios -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando node-fetch
-npm i node-fetch@2clear6clear1 - npm -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando requests
-npm i requests -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando request
-npm install request --save -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando cfonts
-npm i cfonts -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando spinnies
-npm i spinnies    > /dev/null 2>&1
-clear
-echo
-echo    Instalando img-to-base64
-npm i image-to-base64 -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando removeclearbg
-npm i removeclearbg -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando fluent-ffmpeg
-npm i fluent-ffmpeg -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando @adiwajshing/baileys
-npm i @adiwajshing/baileys -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando performance-now
-npm i performance-now -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando form-data
-npm i form-data -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando yt-search
-npm i yt-search -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando google-it
-npm i google-it@1clear6clear1 -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando g-i-s
-npm i g-i-s -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando hxz-api
-npm i hxz-api -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando cheerio
-npm i cheerio -y    > /dev/null 2>&1
-clear
-echo
-echo    Instalando qrcode
-npm i qrcode -y    > /dev/null 2>&1
-clear
+apt-get install nodejs
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+apt-get install nmap
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install -g npm@6 -y 
+npm install toml -y
+npm i crypto-js -y
+npm i moment-timezone -y
+npm i axios -y
+npm i node-fetch@2.6.1 - npm -y
+npm i requests -y
+npm install request --save -y
+npm i cfonts -y
+npm i spinnies
+npm i image-to-base64 -y
+npm i remove.bg -y
+npm i fluent-ffmpeg -y
+npm i @adiwajshing/baileys -y
+npm i performance-now -y
+npm i form-data -y
+npm i yt-search -y
+npm i google-it@1.6.1 -y
+npm i g-i-s -y
+npm i hxz-api -y
+npm i cheerio -y
+npm i qrcode -y
 
 echo -e '\e[1;32m
 Descarga Completada al 100%
